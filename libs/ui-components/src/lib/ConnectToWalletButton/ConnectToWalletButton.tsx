456d86a87cd1bccf39ce340fb392d51902d3d315
takahirohonda <thon6thon6@hotmail.com>
2024-12-11 16:04:42 +1100

    More updates

--- libs/ui-components/src/lib/ConnectToWalletButton/ConnectToWalletButton.tsx
+++ libs/ui-components/src/lib/ConnectToWalletButton/ConnectToWalletButton.tsx
@@ -1,12 +1,13 @@
 import { useDisclosure } from '@nextui-org/react'
 import { GradientButton } from './GradientButton'
-import { ConnectToWalletModal } from 'utils-wagmi'
+import { ConnectToWalletModal } from '../Modals/ConnectedAccountInfoModal/ConnectedAccountInfoModal'
+
 export const ConnectToWalletButton = () => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure()
   return (
     <>
-      <GradientButton>Connect To Wallet</GradientButton>
-      <ConnectToWalletModal />
+      <GradientButton onClick={onOpen}>Connect To Wallet</GradientButton>
+      <ConnectToWalletModal isOpen={isOpen} onOpenChange={onOpenChange} />
     </>
   )
 }
