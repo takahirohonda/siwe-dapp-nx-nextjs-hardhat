d5aebe6705d0ef9384e59e6c6201234e3e0d083f
takahirohonda <thon6thon6@hotmail.com>
2024-12-15 12:06:15 +1100

    chore: fix api type

--- apps/next-gb/src/app/api/user-info/route.ts
+++ apps/next-gb/src/app/api/user-info/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { auth } from '../../../next-auth/auth'
 
-export const GET = auth(function GET(req: Request) {
+export const GET = auth(function GET(req: Request & { auth?: any }) {
   if (req.auth) return NextResponse.json(req.auth)
   return NextResponse.json({ message: 'Not authenticated' }, { status: 401 })
 })
