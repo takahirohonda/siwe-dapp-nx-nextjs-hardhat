7593a25830ec66943ae8b85ed3a1a562bc1a528c
takahirohonda <thon6thon6@hotmail.com>
2025-01-18 15:00:27 +1100

    feat: add new favicons

--- apps/next-gb/src/app/layout.tsx
+++ apps/next-gb/src/app/layout.tsx
@@ -5,6 +5,87 @@ import { AppWrapper } from '../components/AppWrapper/AppWrapper'
 export const metadata = {
   title: 'Gambla 🙄',
   description: 'Be a crypto gladiator...',
+  icons: {
+    apple: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '57x57',
+        url: '/favicon/apple-icon-57x57.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '60x60',
+        url: '/favicon/apple-icon-60x60.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '72x72',
+        url: '/favicon/apple-icon-72x72.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '76x76',
+        url: '/favicon/apple-icon-76x76.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '114x114',
+        url: '/favicon/apple-icon-114x114.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '120x120',
+        url: '/favicon/apple-icon-120x120.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '144x144',
+        url: '/favicon/apple-icon-144x144.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '152x152',
+        url: '/favicon/apple-icon-152x152.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        url: '/favicon/apple-icon-180x180.png',
+      },
+    ],
+    other: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        url: '/favicon/android-icon-192x192.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/favicon/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '96x96',
+        url: '/favicon/favicon-96x96.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/favicon/favicon-16x16.png',
+      },
+    ],
+  },
+  manifest: '/favicon/manifest.json',
+  themeColor: '#ffffff',
+  msapplication: {
+    TileColor: '#ffffff',
+    TileImage: '/favicon/ms-icon-144x144.png',
+  },
 }
 
 export default function RootLayout({
